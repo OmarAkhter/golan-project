ee62f1956b5f43cf0eeaf0cd708a73f1ea5763ae
OmarAkhter <umarakhter757@gmail.com>
2025-12-29 23:02:11 +0500

    first commit

---
+++ cmd/students-api/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Entry point for the students API application.
+	fmt.Println("HI runngin")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OmarAkhter/golan-project
+
+go 1.25.5
